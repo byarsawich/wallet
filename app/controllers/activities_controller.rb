b90b03faa908b1631bf643171fb5ae07fb06c729
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-03-02 17:04:27 -0500

    submit button on form changes text based on action and reciever field will be autofilled on edit action.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -72,6 +72,7 @@ class ActivitiesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_activity
       @activity = Activity.find(params[:id])
+      @reciever = Reciever.find(@activity.reciever_id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
